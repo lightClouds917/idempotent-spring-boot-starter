ecf6be0691d913f1947fcd2e68c121ece01b36f7
汪忠祥 <1186355422@qq.com>
2019-12-22 17:06:06 +0800

    add:init

---
+++ src/test/java/com/java4all/IdempotentSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java4all;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotentSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
