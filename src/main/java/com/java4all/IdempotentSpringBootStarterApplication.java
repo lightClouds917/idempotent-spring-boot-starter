ed7970886a1ff568d4bc969c6ccc5572a6a10c77
汪忠祥 <1186355422@qq.com>
2019-12-22 21:51:41 +0800

    add redission

--- src/main/java/com/java4all/IdempotentSpringBootStarterApplication.java
+++ src/main/java/com/java4all/IdempotentSpringBootStarterApplication.java
@@ -3,6 +3,10 @@ package com.java4all;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ITyunqing
+ * @email 1186355422@qq.com
+ */
 @SpringBootApplication
 public class IdempotentSpringBootStarterApplication {
 
---
+++ src/main/java/com/java4all/config/IdempotentProperties.java
@@ -0,0 +1,9 @@
+package com.java4all.config;
+
+/**
+ * @author ITyunqing
+ * @email 1186355422@qq.com
+ */
+public class IdempotentProperties {
+
+}
